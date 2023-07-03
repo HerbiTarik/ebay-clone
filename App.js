d459ebe4eee09a1ee9e358d0d4229b66b4c8f053
Herbi Tarik <herbitarik2509@gmail.com>
2023-07-03 17:29:56 +0200

    Insatall and add Drawer navigation to the code

--- App.js
+++ App.js
@@ -1,42 +1,22 @@
-import { StatusBar } from 'expo-status-bar'
-import { ScrollView, StyleSheet } from 'react-native'
-import { useCallback, useEffect, useState } from 'react'
-import Entypo from '@expo/vector-icons/Entypo'
-import * as SplashScreen from 'expo-splash-screen'
-import * as Font from 'expo-font'
-import Navbar from './components/Navbar'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { color } from './utils/colors'
-import SearchBar from './components/SearchBar'
-import Menu from './components/Menu'
-import Connection from './components/Connection'
-import Product from './components/Product'
-import Categorie from './components/Categorie'
-import LikePage from './components/LikePage'
-export default function App() {
-    const [appIsReady, setAppIsReady] = useState(false)
+import Accueil from './screens/Accueil'
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 
+const Drawer = createDrawerNavigator()
+
+export default function App() {
     return (
-        <>
-            <SafeAreaView style={styles.container}>
-                <StatusBar style="light" />
-                <Navbar />
-                <ScrollView>
-                    <SearchBar />
-                    <Menu />
-                    <Connection />
-                    <Product />
-                    <Categorie />
-                    <LikePage />
-                </ScrollView>
-            </SafeAreaView>
-        </>
+        <NavigationContainer>
+            <Drawer.Navigator
+                initialRouteName="Home"
+                useLegacyImplementation={true}
+                screenOptions={{
+                    headerShown: false,
+                }}
+            >
+                <Drawer.Screen name="Home" component={Accueil} />
+            </Drawer.Navigator>
+        </NavigationContainer>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: color.primary,
-    },
-})
---
+++ screens/Accueil.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { ScrollView, StyleSheet } from 'react-native'
+import SearchBar from '../components/SearchBar'
+import Menu from '../components/Menu'
+import Connection from '../components/Connection'
+import Product from '../components/Product'
+import Categorie from '../components/Categorie'
+import LikePage from '../components/LikePage'
+import Navbar from '../components/Navbar'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { color } from '../utils/colors'
+import { StatusBar } from 'expo-status-bar'
+
+const Accueil = () => {
+    return (
+        <>
+            <SafeAreaView style={styles.container}>
+                <StatusBar style="light" />
+                <Navbar />
+                <ScrollView>
+                    <SearchBar />
+                    <Menu />
+                    <Connection />
+                    <Product />
+                    <Categorie />
+                    <LikePage />
+                </ScrollView>
+            </SafeAreaView>
+        </>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: color.primary,
+    },
+})
+
+export default Accueil
